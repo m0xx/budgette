03a28cb0823d3f5d58de50d0947c57d8bc86842e
Maxime <maxime.morin2@gmail.com>
2017-10-31 20:49:37 -0400

    create initial implementation

---
+++ bin/index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const program = require('commander');
+const fs = require('fs');
+const moment = require('moment');
+
+const yamlLoader = require('./../lib/yaml-loader');
+const consoleRenderer = require('./../lib/console-renderer');
+const budgette = require('./../lib');
+
+const defaultFilePath = path.join(process.cwd(), 'budgette.yaml');
+const defaultStart = moment().format('YYYY-MM-DD');
+const defaultEnd = moment().add(1, 'year').format('YYYY-MM-DD');
+
+function parseMoment(v, memo) {
+    //TODO: better date format handling, error is ugly
+    const mDate = moment(v);
+    if(!mDate.isValid()) {
+        console.log(`Not a valid moment format: ${v}`)
+        process.exit(1);
+    }
+
+    return mDate.format('YYYY-MM-DD');
+}
+
+program
+    .option(
+        '-f, --file [file]',
+        `Specify file to load [${defaultFilePath}]`,
+        defaultFilePath
+    )
+    .option(
+        '-s, --start [start]',
+        `Start date [${defaultStart}]`,
+        parseMoment,
+        defaultStart
+    )
+    .option(
+        '-e, --end [end]',
+        `End date [${defaultEnd}]`,
+        parseMoment,
+        defaultEnd
+    )
+    .parse(process.argv);
+
+const filePath = program.file;
+
+try {
+    fs.readFileSync(filePath);
+} catch (err) {
+    console.log(`Error accessing file: ${program.filePath}`);
+    process.exit(1);
+}
+
+const mStart = moment(program.start);
+const mEnd = moment(program.end);
+
+budgette.generateTransactions({
+    loader: yamlLoader(filePath),
+    renderer: consoleRenderer(),
+    start: mStart.toDate(),
+    end: mEnd.toDate()
+})
